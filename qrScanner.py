244bad9560f0f25896eb185dff440d9d96e58a17
unknown <lourdes.deguia14@gmail.com>
2022-02-13 21:11:25 +0800

    added polylines and function to write files

--- qrScanner.py
+++ qrScanner.py
@@ -1,17 +1,34 @@
+from unicodedata import name
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-#testEx = decode(cv2.imread("frame.png"))
+#Write into .txt files
+def writeFile():
+    name = results[0].split()
+    output = open(name + ".txt", 'w')
+    output.write(results)
+    output.close()
 
 vidCam = cv2.VideoCapture(0)
 vidCam.set(3, 640)
-vidCam.set(4, 480)
+#vidCam.set(4, 480)
 
 while True:
-    success, img = vidCam.read()
-    for qrcode in decode(img):
-        results = qrcode.data.decode('utf-8')
-        print(results)
-    cv2.imshow("QR Scanner", img)
+    ret, frame = vidCam.read()
+    for qrcode in decode(frame):
+        data = qrcode.data.decode('utf-8')
+        results = data.split(",")
+        print(data)
+        #writeFile()
+        point = np.array([qrcode.polygon], np.int32).reshape((-1, 1, 2))
+        cv2.polylines(frame, [point], True, (230,230,250), 5)
+
+    cv2.imshow("QR Scanner", frame)
     cv2.waitKey(1)
+    #Press (Q) button to exit camera
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vidCam.release()
+cv2.destroyAllWindows()
