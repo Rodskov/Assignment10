2c9384d0b89fe9ca360ea480e3f91c24e1b136c9
unknown <lourdes.deguia14@gmail.com>
2022-02-13 21:43:47 +0800

    completed function to write txt files

--- qrScanner.py
+++ qrScanner.py
@@ -1,26 +1,31 @@
-from unicodedata import name
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+from datetime import datetime
 
 #Write into .txt files
 def writeFile():
-    name = results[0].split()
+    name = "".join(results[0].split())
+    date = datetime.today()
     output = open(name + ".txt", 'w')
-    output.write(results)
+
+    output.write(f"Name: {results[0]}\n")
+    output.write(f"Address: {results[1]}\n")
+    output.write(f"Age: {results[2]}\n")
+    output.write(f"Contact Number: {results[3]}\n")
+    output.write(f"Date and Time: {date}")
+
     output.close()
 
 vidCam = cv2.VideoCapture(0)
 vidCam.set(3, 640)
-#vidCam.set(4, 480)
 
 while True:
     ret, frame = vidCam.read()
     for qrcode in decode(frame):
         data = qrcode.data.decode('utf-8')
         results = data.split(",")
-        print(data)
-        #writeFile()
+        writeFile()
         point = np.array([qrcode.polygon], np.int32).reshape((-1, 1, 2))
         cv2.polylines(frame, [point], True, (230,230,250), 5)
 
